3339eee8fad4bf56f48a9458dd803c213b7b563a
javabear3388 <109869542+javabear3388@users.noreply.github.com>
2023-06-20 20:44:29 -0400

    added rds

---
+++ rds/main.tf
@@ -0,0 +1,17 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "javabear-corp"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+
+resource "aws_db_subnet_group" "default" {
+  name       = "project-db-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+}
+
